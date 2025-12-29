c32ac90e5d29bcc554c0aa6e9ccd670a98249e19
Acrobtw <acrobtw@gmail.com>
2025-12-29 21:45:59 +0200

    added annotaions

---
+++ src/main/java/com/example/store/StripePaymentService.java
@@ -0,0 +1,13 @@
+package com.example.store;
+
+import org.springframework.stereotype.Service;
+
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIPE");
+        System.out.println(" Amount: " + amount + " paid.");
+    }
+    
+}
